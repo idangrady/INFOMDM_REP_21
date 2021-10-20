4aa329f436f0c3886def0a027412a9ee9ed8513c
idangrady <63195210+idangrady@users.noreply.github.com>
2021-10-20 16:09:20 +0200

    Get_Data

--- Assignment_2/Main.py
+++ Assignment_2/Main.py
@@ -14,5 +14,31 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import cross_val_score
 from sklearn.metrics import mean_absolute_error 
 from sklearn.feature_selection import chi2
-import  sklearn.metrics.precision_recall_fscore_support as matrix_recall_precision
+#import  sklearn.metrics.precision_recall_fscore_support as matrix_recall_precision
+import itertools
+import os
 
+
+# =============================================================================
+# 
+# def automated_concat_file(path):
+#     filelist = []
+# 
+#     filenames = ['file1.txt', 'file2.txt', ...]
+#     with open('path/to/output/file', 'w') as outfile:
+#         for line in itertools.chain.from_iterable(itertools.imap(open, filnames)):
+#             outfile.write(line)
+# 
+# =============================================================================
+
+def try_(path):
+    filelist = []
+
+    for root, dirs, files in os.walk(path):
+    	for file in files:
+            #append the file name to the list
+    		filelist.append(os.path.join(root,file))
+    return(filelist)
+
+
+print(try_("Data"))
