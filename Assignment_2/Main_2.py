bed90d7e770b03dda0b42c4bfbcdd8015b2a9681
idangrady <63195210+idangrady@users.noreply.github.com>
2021-10-20 16:40:49 +0200

    Create Main_2.py

---
+++ Assignment_2/Main_2.py
@@ -0,0 +1,70 @@
+"""
+Assignment _2
+"""
+#Imports
+
+#Algorithms
+import numpy as np
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model  import LogisticRegression
+from sklearn.model_selection import GridSearchCV
+
+# Analysis
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import mean_absolute_error 
+from sklearn.feature_selection import chi2
+#import  sklearn.metrics.precision_recall_fscore_support as matrix_recall_precision
+import itertools
+import os
+
+
+
+def automated_concat_file(files):
+    filelist = []
+    
+    for file in files:
+        with open('result.txt', 'w') as result:
+            result.write(str(file)+'\n')
+            
+    return result
+
+   
+def try_(path):
+    filelist = []
+    txt_= []
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            filelist.append(os.path.join(root,file))
+            txt_.append(automated_concat_file(filelist))
+        filelist = []
+    return(txt_)
+
+
+def try_2(path):
+    reviews = {}
+    testlist = []
+    filelist = []
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            filelist.append(os.path.join(root,file))
+            #append the file name to the list
+            filelist.append(os.path.join(root,file))
+            with open(os.path.join(root, file), 'r') as f:
+                text = f.read()
+                testlist.append(text)
+                if 'truth' in root:
+                    print('added 1')
+                    reviews[text] = 1
+                else:
+                    reviews[text] = 0
+    return(filelist)
+
+
+
+
+
+#print(try_("Data"))
+
+print(try_2("Data/negative_polarity"))
+#txt_.append(automated_concat_file(filelist))
